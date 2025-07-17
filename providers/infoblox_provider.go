9b4e93d199749435764e804a028e099a9d81e09e
Simon Anderson <simon.anderson111@gmail.com>
2025-07-17 23:03:59 +1000

    Initial provider struct done

--- providers/infoblox_provider.go
+++ providers/infoblox_provider.go
@@ -4,9 +4,38 @@ import (
 	"github.com/boltKrank/terraformer/terraformutils"
 )
 
+/*
+See: https://github.com/infobloxopen/terraform-provider-infoblox/blob/master/infoblox/provider.go
+
+
+PORT
+SSLMODE
+CONNECT_TIMEOUT
+POOL_CONNECTIONS
+WAPI_VERSION
+*/
+
 type InfobloxProvider struct {
 	terraformutils.Provider
+	server           string
+	username         string
+	password         string
+	port             string
+	sslmode          string
+	connect_timeout  string
+	pool_connections string
+	wapi_version     string
 }
+
+/* AWS example
+
+type AWSProvider struct { //nolint
+	terraformutils.Provider
+	region  string
+	profile string
+}
+
+*/
 
 /*
 func (p *InfobloxProvider) Init(args []string) error {

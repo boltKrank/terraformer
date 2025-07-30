d6044f599fe2ebb94494430e3af3c5e008b7bdda
Simon Anderson <simon.anderson111@gmail.com>
2025-07-30 23:59:15 +1000

    gitignore update

--- providers/infoblox/record_a_service.go
+++ providers/infoblox/record_a_service.go
@@ -10,9 +10,13 @@ import (
 )
 
 type ARecord struct {
-	Name     string `json:"name"`
-	IPv4Addr string `json:"ipv4addr"`
-	View     string `json:"view"`
+	Name         string            `json:"name"`
+	IPv4Addr     string            `json:"ipv4addr"`
+	View         string            `json:"view"`
+	TTL          *int              `json:"ttl,omitempty"`
+	Comment      string            `json:"comment,omitempty"`
+	ExtAttrs     map[string]string `json:"extattrs,omitempty"`
+	ExtAttrsJSON string            // compute JSON string if needed
 }
 
 type ARecordGenerator struct {
@@ -25,7 +29,7 @@ func (g *ARecordGenerator) InitResources() error {
 	user := os.Getenv("INFOBLOX_USERNAME")
 	pass := os.Getenv("INFOBLOX_PASSWORD")
 
-	url := fmt.Sprintf("https://%s/wapi/v2.10/record:a?_return_fields=name,ipv4addr,view", host)
+	url := fmt.Sprintf("https://%s/wapi/v2.13/record:a", host)
 
 	req, _ := http.NewRequest("GET", url, nil)
 	req.SetBasicAuth(user, pass)
@@ -46,18 +50,30 @@ func (g *ARecordGenerator) InitResources() error {
 
 	for _, rec := range result.Records {
 		id := fmt.Sprintf("%s_%s", rec.Name, rec.IPv4Addr)
+		attrs := map[string]string{
+			"fqdn":    rec.Name,
+			"address": rec.IPv4Addr,
+			"view":    rec.View,
+		}
+		if rec.TTL != nil {
+			attrs["ttl"] = fmt.Sprintf("%d", *rec.TTL)
+		}
+		if rec.Comment != "" {
+			attrs["comment"] = rec.Comment
+		}
+		// Optionally include ext_attrs if available
+		if len(rec.ExtAttrs) > 0 {
+			attrs["ext_attrs"] = rec.ExtAttrsJSON
+		}
+
 		g.Resources = append(g.Resources, terraformutils.NewResource(
 			id,
 			id,
 			"infoblox_a_record",
 			"infoblox",
-			map[string]string{
-				"name":     rec.Name,
-				"ipv4addr": rec.IPv4Addr,
-				"view":     rec.View,
-			},
-			[]string{},               // ignored keys
-			map[string]interface{}{}, // additional fields
+			attrs,
+			[]string{},
+			map[string]interface{}{},
 		))
 	}
 
